26907d464cf2c16e15916c298f7b12e3e8b61be0
Jimmy Hu <jimmy.hu@dexon.org>
2018-10-31 17:22:29 +0800

    vendor: sync consensus core and fix conflic

--- dex/network.go
+++ dex/network.go
@@ -1,3 +1,20 @@
+// Copyright 2018 The dexon-consensus-core Authors
+// This file is part of the dexon-consensus-core library.
+//
+// The dexon-consensus-core library is free software: you can redistribute it
+// and/or modify it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
+//
+// The dexon-consensus-core library is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+// General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the dexon-consensus-core library. If not, see
+// <http://www.gnu.org/licenses/>.
+
 package dex
 
 import (
@@ -17,6 +34,10 @@ func NewDexconNetwork(pm *ProtocolManager) *DexconNetwork {
 
 // PullBlocks tries to pull blocks from the DEXON network.
 func (n *DexconNetwork) PullBlocks(hashes coreCommon.Hashes) {
+}
+
+// PullVotes tries to pull votes from the DEXON network.
+func (n *DexconNetwork) PullVotes(pos types.Position) {
 }
 
 // BroadcastVote broadcasts vote to all nodes in DEXON network.
