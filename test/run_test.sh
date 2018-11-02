33db8ae235b072d4b6483ea9fea68e19e5383dce
Wei-Ning Huang <w@dexon.org>
2018-11-02 21:21:13 +0800

    test: save log to unique dir to preserve logs accross tests

--- test/run_test.sh
+++ test/run_test.sh
@@ -6,11 +6,14 @@ pkill -9 -f gdex
 
 bootnode -nodekey bootnode.key --verbosity=9 > bootnode.log 2>&1 &
 
+logsdir=$PWD/log-$(date '+%Y-%m-%d-%H:%M:%S')
+mkdir $logsdir
+
 for i in $(seq 0 3); do
   datadir=$PWD/Dexon.$i
   rm -rf $datadir
   $GDEX --datadir=$datadir init genesis.json
-  $GDEX --verbosity=4 --gcmode=archive --datadir=$datadir --nodekey=test$i.nodekey --port=$((28000 + $i)) --rpc --rpcaddr=0.0.0.0 --rpcport=$((8545 + $i * 2)) --rpcapi=eth,net,web3,debug --ws --wsapi=eth,net,web3,debug --wsaddr=0.0.0.0 --wsport=$((8546 + $i * 2)) --wsorigins='*' --rpcvhosts='*' --rpccorsdomain="*" > gdex.$i.log 2>&1 &
+  $GDEX --verbosity=4 --gcmode=archive --datadir=$datadir --nodekey=test$i.nodekey --port=$((28000 + $i)) --rpc --rpcaddr=0.0.0.0 --rpcport=$((8545 + $i * 2)) --rpcapi=eth,net,web3,debug --ws --wsapi=eth,net,web3,debug --wsaddr=0.0.0.0 --wsport=$((8546 + $i * 2)) --wsorigins='*' --rpcvhosts='*' --rpccorsdomain="*" > $logsdir/gdex.$i.log 2>&1 &
 done
 
-tail -f gdex.*.log
+tail -f $logsdir/gdex.*.log
