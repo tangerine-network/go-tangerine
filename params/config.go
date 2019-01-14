b78a7e2cdfb2240a5401d430c165ca3be100e03c
Wei-Ning Huang <w@dexon.org>
2019-01-14 21:09:28 +0800

    params: update testnet dMoment

--- params/config.go
+++ params/config.go
@@ -90,7 +90,7 @@ var (
 	// TestnetChainConfig contains the chain parameters to run a node on the Taiwan test network.
 	TestnetChainConfig = &ChainConfig{
 		ChainID:             big.NewInt(238),
-		DMoment:             1547462100,
+		DMoment:             1547471400,
 		HomesteadBlock:      big.NewInt(0),
 		DAOForkBlock:        nil,
 		DAOForkSupport:      true,
