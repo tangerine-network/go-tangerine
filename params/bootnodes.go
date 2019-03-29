d1524ef0d32ecf12a92b1c0e18d6dd561798b1c8
Wei-Ning Huang <w@dexon.org>
2019-03-29 22:45:51 +0800

    params: update bootnode (#316)

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -25,19 +25,21 @@ var MainnetBootnodes = []string{
 // TestnetBootnodes are the enode URLs of the P2P bootstrap nodes running on the
 // Taiwan test network.
 var TestnetBootnodes = []string{
-	"enode://0478aa13c91aa0db8e93b668313b7eb0532fbdb24f64772375373b14dbe326c238ad09ab4469f6442c9a9753f1275aeec2e531912c14a958ed1feb4ae7e227ef@35.234.27.122:30301",
+	"enode://801efa60c24a34a5025bb4472f25e65de951657d2944afe12371c167ba14b9da706dc6e584a2cd22faf98d4975edfc624f63da7accc188ea946a0e2c3e0df132@35.201.155.158:30301",
+	"enode://35d0154883fb8571a49951a24a589f16adbd693c06a36e472c4a8c29ed7400d87359e24e54140016ec88106ba12d5678fafc0c86400ed5cf78ba089717c4dc6c@34.73.40.90:30301",
 }
 
 // TaipeiBootnodes are the enode URLs of the P2P bootstrap nodes running on the
 // Taipei test network.
 var TaipeiBootnodes = []string{
-	"enode://0478aa13c91aa0db8e93b668313b7eb0532fbdb24f64772375373b14dbe326c238ad09ab4469f6442c9a9753f1275aeec2e531912c14a958ed1feb4ae7e227ef@34.80.32.107:30301",
+	"enode://66f54114842accd1e09620d804e114f3f967193bf37a391fed44ea208a5280198d183f1e7297c59719c2b2426fb0e10a4da5f820a2262993f698e16666427224@104.199.141.226:30301",
+	"enode://8aafeabce292097e68da9e84e57453af2229340257931e2fecfa527cb77189dd8a70144854e279918ad4c631674dfbaf0e3058dab451d5964d7c9fdc6c79cbcc@35.196.137.55:30301",
 }
 
 // YilanBootnodes are the enode URLs of the P2P bootstrap nodes running on the
 // Yilan test network.
 var YilanBootnodes = []string{
-	"enode://0478aa13c91aa0db8e93b668313b7eb0532fbdb24f64772375373b14dbe326c238ad09ab4469f6442c9a9753f1275aeec2e531912c14a958ed1feb4ae7e227ef@34.80.4.56:30301",
+	"enode://242df07a1fa3c337b50c0d0f4d7d305c00f2610383a8e468567dba53d77c5e67213e85f00d884baef96dfc726da5f5a4de7c4ef3e295fceb543c8ed4337b599f@34.80.4.56:30301",
 }
 
 // DiscoveryV5Bootnodes are the enode URLs of the P2P bootstrap nodes for the
