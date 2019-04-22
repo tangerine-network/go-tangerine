ac265bc2a70ca39d12c51b72bd2bca6ae5021da8
Wei-Ning Huang <w@dexon.org>
2019-04-22 15:26:09 +0800

    core: update mainnet genesis alloc (#384)

--- params/config.go
+++ params/config.go
@@ -26,7 +26,7 @@ import (
 
 // Genesis hashes to enforce below configs on.
 var (
-	MainnetGenesisHash = common.HexToHash("0xb11ab63566eb5b6179938613ed061af3a5242b847e5762b70fa9e10f10a0bcc3")
+	MainnetGenesisHash = common.HexToHash("0xb6ed1ad6b6956e91f5c2dfe4f94f6fb3d2c708a5e25161210995bb5d1f667e4b")
 	TestnetGenesisHash = common.HexToHash("0x7d8700a7a731162880adff4f21398a901c0b75d907bec8f4eac51460f94cb846")
 	TaipeiGenesisHash  = common.HexToHash("0x5929cb70fe4ba22dce821b2efca737a1874a0f5a34f3ffb9a1e157516622e20b")
 	YilanGenesisHash   = common.HexToHash("0xdcdafc044c24d728c6149ecfada746d8de6e59fc5d18063caf7950badc1df12e")
