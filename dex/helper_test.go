HelloWorldDev/dexon

dex: redesign p2p network topology
- Let p2p server support direct connection and group connection.
- Introduce node meta table to maintain IP of all nodes in node set,
  in memory and let nodes in the network can sync this table.
- Let peerSet able to manage direct connections to notary set and dkg set.

The mechanism to refresh the network topology when configuration round
change is not done yet.

dex/helper_test.go
@@ -48,24 +48,47 @@ var (
 
 // testP2PServer is a fake, helper p2p server for testing purposes.
 type testP2PServer struct {
-	added   chan *discover.Node
-	removed chan *discover.Node
+	mu     sync.Mutex
+	self   *discover.Node
+	direct map[discover.NodeID]*discover.Node
+	group  map[string][]*discover.Node
+}
+
+func newTestP2PServer(self *discover.Node) *testP2PServer {
+	return &testP2PServer{
+		self:   self,
+		direct: make(map[discover.NodeID]*discover.Node),
+		group:  make(map[string][]*discover.Node),
+	}
 }
 
 func (s *testP2PServer) Self() *discover.Node {
-	return &discover.Node{}
+	return s.self
 }
 
-func (s *testP2PServer) AddNotaryPeer(node *discover.Node) {
-	if s.added != nil {
-		s.added <- node
-	}
+func (s *testP2PServer) AddDirectPeer(node *discover.Node) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.direct[node.ID] = node
 }
 
-func (s *testP2PServer) RemoveNotaryPeer(node *discover.Node) {
-	if s.removed != nil {
-		s.removed <- node
-	}
+func (s *testP2PServer) RemoveDirectPeer(node *discover.Node) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	delete(s.direct, node.ID)
+}
+
+func (s *testP2PServer) AddGroup(
+	name string, nodes []*discover.Node, num uint64) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.group[name] = nodes
+}
+
+func (s *testP2PServer) RemoveGroup(name string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	delete(s.group, name)
 }
 
 // newTestProtocolManager creates a new protocol manager for testing purposes,
@@ -88,7 +111,7 @@ func newTestProtocolManager(mode downloader.SyncMode, blocks int, generator func
 		panic(err)
 	}
 
-	pm, err := NewProtocolManager(gspec.Config, mode, DefaultConfig.NetworkId, evmux, &testTxPool{added: newtx}, engine, blockchain, db)
+	pm, err := NewProtocolManager(gspec.Config, mode, DefaultConfig.NetworkId, evmux, &testTxPool{added: newtx}, engine, blockchain, db, &testGovernance{})
 	if err != nil {
 		return nil, nil, err
 	}
@@ -155,6 +178,29 @@ func newTestTransaction(from *ecdsa.PrivateKey, nonce uint64, datasize int) *typ
 	tx := types.NewTransaction(nonce, common.Address{}, big.NewInt(0), 100000, big.NewInt(0), make([]byte, datasize))
 	tx, _ = types.SignTx(tx, types.HomesteadSigner{}, from)
 	return tx
+}
+
+// testGovernance is a fake, helper governance for testing purposes
+type testGovernance struct {
+	getChainNumFunc  func(uint64) uint32
+	getNotarySetFunc func(uint32, uint64) map[string]struct{}
+	getDKGSetFunc    func(uint64) map[string]struct{}
+}
+
+func (g *testGovernance) GetChainNum(round uint64) uint32 {
+	return g.getChainNumFunc(round)
+}
+
+func (g *testGovernance) GetNotarySet(chainID uint32, round uint64) map[string]struct{} {
+	return g.getNotarySetFunc(chainID, round)
+}
+
+func (g *testGovernance) GetDKGSet(round uint64) map[string]struct{} {
+	return g.getDKGSetFunc(round)
+}
+
+func (g *testGovernance) SubscribeNewCRSEvent(ch chan core.NewCRSEvent) event.Subscription {
+	return nil
 }
 
 // testPeer is a simulated peer to allow testing direct network calls.

dex/nodetable.go
@@ -1,0 +1,79 @@
+package dex
+
+import (
+	"net"
+	"sync"
+
+	"github.com/dexon-foundation/dexon/common"
+	"github.com/dexon-foundation/dexon/crypto/sha3"
+	"github.com/dexon-foundation/dexon/event"
+	"github.com/dexon-foundation/dexon/p2p/discover"
+	"github.com/dexon-foundation/dexon/rlp"
+)
+
+type NodeMeta struct {
+	ID        discover.NodeID
+	IP        net.IP
+	UDP       uint16
+	TCP       uint16
+	Timestamp uint64
+	Sig       []byte
+}
+
+func (n *NodeMeta) Hash() (h common.Hash) {
+	hw := sha3.NewKeccak256()
+	rlp.Encode(hw, n)
+	hw.Sum(h[:0])
+	return h
+}
+
+type newMetasEvent struct{ Metas []*NodeMeta }
+
+type nodeTable struct {
+	mu    sync.RWMutex
+	entry map[discover.NodeID]*NodeMeta
+	feed  event.Feed
+}
+
+func newNodeTable() *nodeTable {
+	return &nodeTable{
+		entry: make(map[discover.NodeID]*NodeMeta),
+	}
+}
+
+func (t *nodeTable) Get(id discover.NodeID) *NodeMeta {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+	return t.entry[id]
+}
+
+func (t *nodeTable) Add(metas []*NodeMeta) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
+	var newMetas []*NodeMeta
+	for _, meta := range metas {
+		// TODO: validate the meta
+		if e, ok := t.entry[meta.ID]; ok && e.Timestamp > meta.Timestamp {
+			continue
+		}
+		t.entry[meta.ID] = meta
+		newMetas = append(newMetas, meta)
+	}
+	t.feed.Send(newMetasEvent{newMetas})
+}
+
+func (t *nodeTable) Metas() []*NodeMeta {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+	metas := make([]*NodeMeta, 0, len(t.entry))
+	for _, meta := range t.entry {
+		metas = append(metas, meta)
+	}
+	return metas
+}
+
+func (t *nodeTable) SubscribeNewMetasEvent(
+	ch chan<- newMetasEvent) event.Subscription {
+	return t.feed.Subscribe(ch)
+}

dex/nodetable_test.go
@@ -1,0 +1,93 @@
+package dex
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/dexon-foundation/dexon/common"
+	"github.com/dexon-foundation/dexon/p2p/discover"
+)
+
+func TestNodeTable(t *testing.T) {
+	table := newNodeTable()
+	ch := make(chan newMetasEvent)
+	table.SubscribeNewMetasEvent(ch)
+
+	metas1 := []*NodeMeta{
+		&NodeMeta{ID: randomID()},
+		&NodeMeta{ID: randomID()},
+	}
+
+	metas2 := []*NodeMeta{
+		&NodeMeta{ID: randomID()},
+		&NodeMeta{ID: randomID()},
+	}
+
+	go table.Add(metas1)
+
+	select {
+	case newMetas := <-ch:
+		m := map[common.Hash]struct{}{}
+		for _, meta := range newMetas.Metas {
+			m[meta.Hash()] = struct{}{}
+		}
+
+		if len(m) != len(metas1) {
+			t.Errorf("len mismatch: got %d, want: %d",
+				len(m), len(metas1))
+		}
+
+		for _, meta := range metas1 {
+			if _, ok := m[meta.Hash()]; !ok {
+				t.Errorf("expected meta (%s) not exists", meta.Hash())
+			}
+		}
+	case <-time.After(1 * time.Second):
+		t.Error("did not receive new metas event within one second")
+	}
+
+	go table.Add(metas2)
+	select {
+	case newMetas := <-ch:
+		m := map[common.Hash]struct{}{}
+		for _, meta := range newMetas.Metas {
+			m[meta.Hash()] = struct{}{}
+		}
+
+		if len(m) != len(metas1) {
+			t.Errorf("len mismatch: got %d, want: %d",
+				len(m), len(metas2))
+		}
+
+		for _, meta := range metas2 {
+			if _, ok := m[meta.Hash()]; !ok {
+				t.Errorf("expected meta (%s) not exists", meta.Hash())
+			}
+		}
+	case <-time.After(1 * time.Second):
+		t.Error("did not receive new metas event within one second")
+	}
+
+	var metas []*NodeMeta
+	metas = append(metas, metas1...)
+	metas = append(metas, metas2...)
+	allMetas := table.Metas()
+	if len(allMetas) != len(metas) {
+		t.Errorf("all metas num mismatch: got %d, want %d",
+			len(metas), len(allMetas))
+	}
+
+	for _, m := range metas {
+		if m.Hash() != table.Get(m.ID).Hash() {
+			t.Errorf("meta (%s) mismatch", m.ID.String())
+		}
+	}
+}
+
+func randomID() (id discover.NodeID) {
+	for i := range id {
+		id[i] = byte(rand.Intn(255))
+	}
+	return id
+}

dex/sync.go
@@ -35,6 +35,9 @@ const (
 	// This is the target size for the packs of transactions sent by txsyncLoop.
 	// A pack can get larger than this if a single transactions exceeds this size.
 	txsyncPackSize = 100 * 1024
+
+	// This is the target number for the packs of metas sent by metasyncLoop.
+	metasyncPackNum = 1024
 )
 
 type txsync struct {
@@ -117,6 +120,94 @@ func (pm *ProtocolManager) txsyncLoop() {
 			// Stop tracking peers that cause send failures.
 			if err != nil {
 				pack.p.Log().Debug("Transaction send failed", "err", err)
+				delete(pending, pack.p.ID())
+			}
+			// Schedule the next send.
+			if s := pick(); s != nil {
+				send(s)
+			}
+		case <-pm.quitSync:
+			return
+		}
+	}
+}
+
+type metasync struct {
+	p     *peer
+	metas []*NodeMeta
+}
+
+// syncNodeMetas starts sending all node metas to the given peer.
+func (pm *ProtocolManager) syncNodeMetas(p *peer) {
+	metas := pm.nodeTable.Metas()
+	if len(metas) == 0 {
+		return
+	}
+	select {
+	case pm.metasyncCh <- &metasync{p, metas}:
+	case <-pm.quitSync:
+	}
+}
+
+// metasyncLoop takes care of the initial node meta sync for each new
+// connection. When a new peer appears, we relay all currently node metas.
+// In order to minimise egress bandwidth usage, we send
+// the metas in small packs to one peer at a time.
+func (pm *ProtocolManager) metasyncLoop() {
+	var (
+		pending = make(map[discover.NodeID]*metasync)
+		sending = false               // whether a send is active
+		pack    = new(metasync)       // the pack that is being sent
+		done    = make(chan error, 1) // result of the send
+	)
+
+	// send starts a sending a pack of transactions from the sync.
+	send := func(s *metasync) {
+		// Fill pack with node metas up to the target num.
+		var num int
+		pack.p = s.p
+		pack.metas = pack.metas[:0]
+		for i := 0; i < len(s.metas) && num < metasyncPackNum; i++ {
+			pack.metas = append(pack.metas, s.metas[i])
+			num += 1
+		}
+		// Remove the metas that will be sent.
+		s.metas = s.metas[:copy(s.metas, s.metas[len(pack.metas):])]
+		if len(s.metas) == 0 {
+			delete(pending, s.p.ID())
+		}
+		// Send the pack in the background.
+		s.p.Log().Trace("Sending batch of transactions", "count", len(pack.metas), "bytes", num)
+		sending = true
+		go func() { done <- pack.p.SendNodeMetas(pack.metas) }()
+	}
+
+	// pick chooses the next pending sync.
+	pick := func() *metasync {
+		if len(pending) == 0 {
+			return nil
+		}
+		n := rand.Intn(len(pending)) + 1
+		for _, s := range pending {
+			if n--; n == 0 {
+				return s
+			}
+		}
+		return nil
+	}
+
+	for {
+		select {
+		case s := <-pm.metasyncCh:
+			pending[s.p.ID()] = s
+			if !sending {
+				send(s)
+			}
+		case err := <-done:
+			sending = false
+			// Stop tracking peers that cause send failures.
+			if err != nil {
+				pack.p.Log().Debug("NodeMeta send failed", "err", err)
 				delete(pending, pack.p.ID())
 			}
 			// Schedule the next send.

p2p/dial.go
@@ -65,6 +65,12 @@ func (t TCPDialer) Dial(dest *discover.Node) (net.Conn, error) {
 	return t.Dialer.Dial("tcp", addr.String())
 }
 
+type dialGroup struct {
+	name  string
+	nodes map[discover.NodeID]*discover.Node
+	num   uint64
+}
+
 // dialstate schedules dials and discovery lookups.
 // it get's a chance to compute new tasks on every iteration
 // of the main loop in Server.run.
@@ -78,6 +84,8 @@ type dialstate struct {
 	lookupBuf     []*discover.Node // current discovery lookup results
 	randomNodes   []*discover.Node // filled from Table
 	static        map[discover.NodeID]*dialTask
+	direct        map[discover.NodeID]*dialTask
+	group         map[string]*dialGroup
 	hist          *dialHistory
 
 	start     time.Time        // time when the dialer was first used
@@ -133,6 +141,8 @@ func newDialState(static []*discover.Node, bootnodes []*discover.Node, ntab disc
 		ntab:        ntab,
 		netrestrict: netrestrict,
 		static:      make(map[discover.NodeID]*dialTask),
+		direct:      make(map[discover.NodeID]*dialTask),
+		group:       make(map[string]*dialGroup),
 		dialing:     make(map[discover.NodeID]connFlag),
 		bootnodes:   make([]*discover.Node, len(bootnodes)),
 		randomNodes: make([]*discover.Node, maxdyn/2),
@@ -157,6 +167,23 @@ func (s *dialstate) removeStatic(n *discover.Node) {
 	// This removes a previous dial timestamp so that application
 	// can force a server to reconnect with chosen peer immediately.
 	s.hist.remove(n.ID)
+}
+
+func (s *dialstate) addDirect(n *discover.Node) {
+	s.direct[n.ID] = &dialTask{flags: directDialedConn, dest: n}
+}
+
+func (s *dialstate) removeDirect(n *discover.Node) {
+	delete(s.direct, n.ID)
+	s.hist.remove(n.ID)
+}
+
+func (s *dialstate) addGroup(g *dialGroup) {
+	s.group[g.name] = g
+}
+
+func (s *dialstate) removeGroup(g *dialGroup) {
+	delete(s.group, g.name)
 }
 
 func (s *dialstate) newTasks(nRunning int, peers map[discover.NodeID]*Peer, now time.Time) []task {
@@ -203,6 +230,62 @@ func (s *dialstate) newTasks(nRunning int, peers map[discover.NodeID]*Peer, now 
 			newtasks = append(newtasks, t)
 		}
 	}
+
+	for id, t := range s.direct {
+		err := s.checkDial(t.dest, peers)
+		switch err {
+		case errNotWhitelisted, errSelf:
+			log.Warn("Removing direct dial candidate", "id", t.dest.ID, "addr", &net.TCPAddr{IP: t.dest.IP, Port: int(t.dest.TCP)}, "err", err)
+			delete(s.direct, t.dest.ID)
+		case nil:
+			s.dialing[id] = t.flags
+			newtasks = append(newtasks, t)
+		}
+	}
+
+	// compute connected
+	connected := map[string]map[discover.NodeID]struct{}{}
+	for _, g := range s.group {
+		connected[g.name] = map[discover.NodeID]struct{}{}
+	}
+
+	for id := range peers {
+		for _, g := range s.group {
+			if _, ok := g.nodes[id]; ok {
+				connected[g.name][id] = struct{}{}
+			}
+		}
+	}
+
+	for id := range s.dialing {
+		for _, g := range s.group {
+			if _, ok := g.nodes[id]; ok {
+				connected[g.name][id] = struct{}{}
+			}
+		}
+	}
+
+	groupNodes := map[discover.NodeID]*discover.Node{}
+	for _, g := range s.group {
+		for _, n := range g.nodes {
+			if uint64(len(connected[g.name])) >= g.num {
+				break
+			}
+			err := s.checkDial(n, peers)
+			switch err {
+			case errNotWhitelisted, errSelf:
+				log.Warn("Removing group dial candidate", "id", n.ID, "addr", &net.TCPAddr{IP: n.IP, Port: int(n.TCP)}, "err", err)
+				delete(g.nodes, n.ID)
+			case nil:
+				groupNodes[n.ID] = n
+				connected[g.name][n.ID] = struct{}{}
+			}
+		}
+	}
+	for _, n := range groupNodes {
+		addDial(groupDialedConn, n)
+	}
+
 	// If we don't have any peers whatsoever, try to dial a random bootnode. This
 	// scenario is useful for the testnet (and private networks) where the discovery
 	// table might be full of mostly bad peers, making it hard to find good ones.

tangerine-network/go-tangerine

dex/helper_test.go
@@ -37,7 +37,7 @@ import (
 	"github.com/dexon-foundation/dexon/ethdb"
 	"github.com/dexon-foundation/dexon/event"
 	"github.com/dexon-foundation/dexon/p2p"
-	"github.com/dexon-foundation/dexon/p2p/discover"
+	"github.com/dexon-foundation/dexon/p2p/enode"
 	"github.com/dexon-foundation/dexon/params"
 )
 
@@ -48,24 +48,47 @@ var (
 
 // testP2PServer is a fake, helper p2p server for testing purposes.
 type testP2PServer struct {
-	added   chan *discover.Node
-	removed chan *discover.Node
+	mu     sync.Mutex
+	self   *enode.Node
+	direct map[enode.NodeID]*enode.Node
+	group  map[string][]*enode.Node
 }
 
-func (s *testP2PServer) Self() *discover.Node {
-	return &discover.Node{}
-}
-
-func (s *testP2PServer) AddNotaryPeer(node *discover.Node) {
-	if s.added != nil {
-		s.added <- node
+func newTestP2PServer(self *enode.Node) *testP2PServer {
+	return &testP2PServer{
+		self:   self,
+		direct: make(map[enode.NodeID]*enode.Node),
+		group:  make(map[string][]*enode.Node),
 	}
 }
 
-func (s *testP2PServer) RemoveNotaryPeer(node *discover.Node) {
-	if s.removed != nil {
-		s.removed <- node
-	}
+func (s *testP2PServer) Self() *enode.Node {
+	return s.self
+}
+
+func (s *testP2PServer) AddDirectPeer(node *enode.Node) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.direct[node.ID] = node
+}
+
+func (s *testP2PServer) RemoveDirectPeer(node *enode.Node) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	delete(s.direct, node.ID)
+}
+
+func (s *testP2PServer) AddGroup(
+	name string, nodes []*enode.Node, num uint64) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.group[name] = nodes
+}
+
+func (s *testP2PServer) RemoveGroup(name string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	delete(s.group, name)
 }
 
 // newTestProtocolManager creates a new protocol manager for testing purposes,
@@ -88,11 +111,11 @@ func newTestProtocolManager(mode downloader.SyncMode, blocks int, generator func
 		panic(err)
 	}
 
-	pm, err := NewProtocolManager(gspec.Config, mode, DefaultConfig.NetworkId, evmux, &testTxPool{added: newtx}, engine, blockchain, db)
+	pm, err := NewProtocolManager(gspec.Config, mode, DefaultConfig.NetworkId, evmux, &testTxPool{added: newtx}, engine, blockchain, db, &testGovernance{})
 	if err != nil {
 		return nil, nil, err
 	}
-	pm.Start(&testP2PServer{}, 1000)
+	pm.Start(newTestP2PServer(&enode.Node{}), 1000)
 	return pm, db, nil
 }
 
@@ -157,6 +180,29 @@ func newTestTransaction(from *ecdsa.PrivateKey, nonce uint64, datasize int) *typ
 	return tx
 }
 
+// testGovernance is a fake, helper governance for testing purposes
+type testGovernance struct {
+	getChainNumFunc  func(uint64) uint32
+	getNotarySetFunc func(uint32, uint64) map[string]struct{}
+	getDKGSetFunc    func(uint64) map[string]struct{}
+}
+
+func (g *testGovernance) GetChainNum(round uint64) uint32 {
+	return g.getChainNumFunc(round)
+}
+
+func (g *testGovernance) GetNotarySet(chainID uint32, round uint64) map[string]struct{} {
+	return g.getNotarySetFunc(chainID, round)
+}
+
+func (g *testGovernance) GetDKGSet(round uint64) map[string]struct{} {
+	return g.getDKGSetFunc(round)
+}
+
+func (g *testGovernance) SubscribeNewCRSEvent(ch chan core.NewCRSEvent) event.Subscription {
+	return nil
+}
+
 // testPeer is a simulated peer to allow testing direct network calls.
 type testPeer struct {
 	net p2p.MsgReadWriter // Network layer reader/writer to simulate remote messaging
@@ -170,7 +216,7 @@ func newTestPeer(name string, version int, pm *ProtocolManager, shake bool) (*te
 	app, net := p2p.MsgPipe()
 
 	// Generate a random id and create the peer
-	var id discover.NodeID
+	var id enode.NodeID
 	rand.Read(id[:])
 
 	peer := pm.newPeer(version, p2p.NewPeer(id, name, nil), net)

dex/nodetable.go
@@ -1,0 +1,79 @@
+package dex
+
+import (
+	"net"
+	"sync"
+
+	"github.com/dexon-foundation/dexon/common"
+	"github.com/dexon-foundation/dexon/crypto/sha3"
+	"github.com/dexon-foundation/dexon/event"
+	"github.com/dexon-foundation/dexon/p2p/enode"
+	"github.com/dexon-foundation/dexon/rlp"
+)
+
+type NodeMeta struct {
+	ID        enode.ID
+	IP        net.IP
+	UDP       int
+	TCP       int
+	Timestamp uint64
+	Sig       []byte
+}
+
+func (n *NodeMeta) Hash() (h common.Hash) {
+	hw := sha3.NewKeccak256()
+	rlp.Encode(hw, n)
+	hw.Sum(h[:0])
+	return h
+}
+
+type newMetasEvent struct{ Metas []*NodeMeta }
+
+type nodeTable struct {
+	mu    sync.RWMutex
+	entry map[enode.ID]*NodeMeta
+	feed  event.Feed
+}
+
+func newNodeTable() *nodeTable {
+	return &nodeTable{
+		entry: make(map[enode.ID]*NodeMeta),
+	}
+}
+
+func (t *nodeTable) Get(id enode.ID) *NodeMeta {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+	return t.entry[id]
+}
+
+func (t *nodeTable) Add(metas []*NodeMeta) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
+	var newMetas []*NodeMeta
+	for _, meta := range metas {
+		// TODO: validate the meta
+		if e, ok := t.entry[meta.ID]; ok && e.Timestamp > meta.Timestamp {
+			continue
+		}
+		t.entry[meta.ID] = meta
+		newMetas = append(newMetas, meta)
+	}
+	t.feed.Send(newMetasEvent{newMetas})
+}
+
+func (t *nodeTable) Metas() []*NodeMeta {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+	metas := make([]*NodeMeta, 0, len(t.entry))
+	for _, meta := range t.entry {
+		metas = append(metas, meta)
+	}
+	return metas
+}
+
+func (t *nodeTable) SubscribeNewMetasEvent(
+	ch chan<- newMetasEvent) event.Subscription {
+	return t.feed.Subscribe(ch)
+}

dex/nodetable_test.go
@@ -1,0 +1,93 @@
+package dex
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/dexon-foundation/dexon/common"
+	"github.com/dexon-foundation/dexon/p2p/enode"
+)
+
+func TestNodeTable(t *testing.T) {
+	table := newNodeTable()
+	ch := make(chan newMetasEvent)
+	table.SubscribeNewMetasEvent(ch)
+
+	metas1 := []*NodeMeta{
+		&NodeMeta{ID: randomID()},
+		&NodeMeta{ID: randomID()},
+	}
+
+	metas2 := []*NodeMeta{
+		&NodeMeta{ID: randomID()},
+		&NodeMeta{ID: randomID()},
+	}
+
+	go table.Add(metas1)
+
+	select {
+	case newMetas := <-ch:
+		m := map[common.Hash]struct{}{}
+		for _, meta := range newMetas.Metas {
+			m[meta.Hash()] = struct{}{}
+		}
+
+		if len(m) != len(metas1) {
+			t.Errorf("len mismatch: got %d, want: %d",
+				len(m), len(metas1))
+		}
+
+		for _, meta := range metas1 {
+			if _, ok := m[meta.Hash()]; !ok {
+				t.Errorf("expected meta (%s) not exists", meta.Hash())
+			}
+		}
+	case <-time.After(1 * time.Second):
+		t.Error("did not receive new metas event within one second")
+	}
+
+	go table.Add(metas2)
+	select {
+	case newMetas := <-ch:
+		m := map[common.Hash]struct{}{}
+		for _, meta := range newMetas.Metas {
+			m[meta.Hash()] = struct{}{}
+		}
+
+		if len(m) != len(metas1) {
+			t.Errorf("len mismatch: got %d, want: %d",
+				len(m), len(metas2))
+		}
+
+		for _, meta := range metas2 {
+			if _, ok := m[meta.Hash()]; !ok {
+				t.Errorf("expected meta (%s) not exists", meta.Hash())
+			}
+		}
+	case <-time.After(1 * time.Second):
+		t.Error("did not receive new metas event within one second")
+	}
+
+	var metas []*NodeMeta
+	metas = append(metas, metas1...)
+	metas = append(metas, metas2...)
+	allMetas := table.Metas()
+	if len(allMetas) != len(metas) {
+		t.Errorf("all metas num mismatch: got %d, want %d",
+			len(metas), len(allMetas))
+	}
+
+	for _, m := range metas {
+		if m.Hash() != table.Get(m.ID).Hash() {
+			t.Errorf("meta (%s) mismatch", m.ID.String())
+		}
+	}
+}
+
+func randomID() (id enode.ID) {
+	for i := range id {
+		id[i] = byte(rand.Intn(255))
+	}
+	return id
+}

dex/sync.go
@@ -25,7 +25,7 @@ import (
 	"github.com/dexon-foundation/dexon/core/types"
 	"github.com/dexon-foundation/dexon/eth/downloader"
 	"github.com/dexon-foundation/dexon/log"
-	"github.com/dexon-foundation/dexon/p2p/discover"
+	"github.com/dexon-foundation/dexon/p2p/enode"
 )
 
 const (
@@ -35,6 +35,9 @@ const (
 	// This is the target size for the packs of transactions sent by txsyncLoop.
 	// A pack can get larger than this if a single transactions exceeds this size.
 	txsyncPackSize = 100 * 1024
+
+	// This is the target number for the packs of metas sent by metasyncLoop.
+	metasyncPackNum = 1024
 )
 
 type txsync struct {
@@ -64,7 +67,7 @@ func (pm *ProtocolManager) syncTransactions(p *peer) {
 // the transactions in small packs to one peer at a time.
 func (pm *ProtocolManager) txsyncLoop() {
 	var (
-		pending = make(map[discover.NodeID]*txsync)
+		pending = make(map[enode.ID]*txsync)
 		sending = false               // whether a send is active
 		pack    = new(txsync)         // the pack that is being sent
 		done    = make(chan error, 1) // result of the send
@@ -117,6 +120,94 @@ func (pm *ProtocolManager) txsyncLoop() {
 			// Stop tracking peers that cause send failures.
 			if err != nil {
 				pack.p.Log().Debug("Transaction send failed", "err", err)
+				delete(pending, pack.p.ID())
+			}
+			// Schedule the next send.
+			if s := pick(); s != nil {
+				send(s)
+			}
+		case <-pm.quitSync:
+			return
+		}
+	}
+}
+
+type metasync struct {
+	p     *peer
+	metas []*NodeMeta
+}
+
+// syncNodeMetas starts sending all node metas to the given peer.
+func (pm *ProtocolManager) syncNodeMetas(p *peer) {
+	metas := pm.nodeTable.Metas()
+	if len(metas) == 0 {
+		return
+	}
+	select {
+	case pm.metasyncCh <- &metasync{p, metas}:
+	case <-pm.quitSync:
+	}
+}
+
+// metasyncLoop takes care of the initial node meta sync for each new
+// connection. When a new peer appears, we relay all currently node metas.
+// In order to minimise egress bandwidth usage, we send
+// the metas in small packs to one peer at a time.
+func (pm *ProtocolManager) metasyncLoop() {
+	var (
+		pending = make(map[enode.ID]*metasync)
+		sending = false               // whether a send is active
+		pack    = new(metasync)       // the pack that is being sent
+		done    = make(chan error, 1) // result of the send
+	)
+
+	// send starts a sending a pack of transactions from the sync.
+	send := func(s *metasync) {
+		// Fill pack with node metas up to the target num.
+		var num int
+		pack.p = s.p
+		pack.metas = pack.metas[:0]
+		for i := 0; i < len(s.metas) && num < metasyncPackNum; i++ {
+			pack.metas = append(pack.metas, s.metas[i])
+			num += 1
+		}
+		// Remove the metas that will be sent.
+		s.metas = s.metas[:copy(s.metas, s.metas[len(pack.metas):])]
+		if len(s.metas) == 0 {
+			delete(pending, s.p.ID())
+		}
+		// Send the pack in the background.
+		s.p.Log().Trace("Sending batch of transactions", "count", len(pack.metas), "bytes", num)
+		sending = true
+		go func() { done <- pack.p.SendNodeMetas(pack.metas) }()
+	}
+
+	// pick chooses the next pending sync.
+	pick := func() *metasync {
+		if len(pending) == 0 {
+			return nil
+		}
+		n := rand.Intn(len(pending)) + 1
+		for _, s := range pending {
+			if n--; n == 0 {
+				return s
+			}
+		}
+		return nil
+	}
+
+	for {
+		select {
+		case s := <-pm.metasyncCh:
+			pending[s.p.ID()] = s
+			if !sending {
+				send(s)
+			}
+		case err := <-done:
+			sending = false
+			// Stop tracking peers that cause send failures.
+			if err != nil {
+				pack.p.Log().Debug("NodeMeta send failed", "err", err)
 				delete(pending, pack.p.ID())
 			}
 			// Schedule the next send.

p2p/dial.go
@@ -64,6 +64,12 @@ func (t TCPDialer) Dial(dest *enode.Node) (net.Conn, error) {
 	return t.Dialer.Dial("tcp", addr.String())
 }
 
+type dialGroup struct {
+	name  string
+	nodes map[enode.ID]*enode.Node
+	num   uint64
+}
+
 // dialstate schedules dials and discovery lookups.
 // it get's a chance to compute new tasks on every iteration
 // of the main loop in Server.run.
@@ -78,6 +84,8 @@ type dialstate struct {
 	lookupBuf     []*enode.Node // current discovery lookup results
 	randomNodes   []*enode.Node // filled from Table
 	static        map[enode.ID]*dialTask
+	direct        map[enode.ID]*dialTask
+	group         map[string]*dialGroup
 	hist          *dialHistory
 
 	start     time.Time     // time when the dialer was first used
@@ -85,6 +93,7 @@ type dialstate struct {
 }
 
 type discoverTable interface {
+	Self() *enode.Node
 	Close()
 	Resolve(*enode.Node) *enode.Node
 	LookupRandom() []*enode.Node
@@ -133,6 +142,8 @@ func newDialState(self enode.ID, static []*enode.Node, bootnodes []*enode.Node, 
 		self:        self,
 		netrestrict: netrestrict,
 		static:      make(map[enode.ID]*dialTask),
+		direct:      make(map[enode.ID]*dialTask),
+		group:       make(map[string]*dialGroup),
 		dialing:     make(map[enode.ID]connFlag),
 		bootnodes:   make([]*enode.Node, len(bootnodes)),
 		randomNodes: make([]*enode.Node, maxdyn/2),
@@ -157,6 +168,23 @@ func (s *dialstate) removeStatic(n *enode.Node) {
 	// This removes a previous dial timestamp so that application
 	// can force a server to reconnect with chosen peer immediately.
 	s.hist.remove(n.ID())
+}
+
+func (s *dialstate) addDirect(n *enode.Node) {
+	s.direct[n.ID()] = &dialTask{flags: directDialedConn, dest: n}
+}
+
+func (s *dialstate) removeDirect(n *enode.Node) {
+	delete(s.direct, n.ID())
+	s.hist.remove(n.ID())
+}
+
+func (s *dialstate) addGroup(g *dialGroup) {
+	s.group[g.name] = g
+}
+
+func (s *dialstate) removeGroup(g *dialGroup) {
+	delete(s.group, g.name)
 }
 
 func (s *dialstate) newTasks(nRunning int, peers map[enode.ID]*Peer, now time.Time) []task {
@@ -196,13 +224,69 @@ func (s *dialstate) newTasks(nRunning int, peers map[enode.ID]*Peer, now time.Ti
 		err := s.checkDial(t.dest, peers)
 		switch err {
 		case errNotWhitelisted, errSelf:
-			log.Warn("Removing static dial candidate", "id", t.dest.ID, "addr", &net.TCPAddr{IP: t.dest.IP(), Port: t.dest.TCP()}, "err", err)
+			log.Warn("Removing static dial candidate", "id", t.dest.ID(), "addr", &net.TCPAddr{IP: t.dest.IP(), Port: t.dest.TCP()}, "err", err)
 			delete(s.static, t.dest.ID())
 		case nil:
 			s.dialing[id] = t.flags
 			newtasks = append(newtasks, t)
 		}
 	}
+
+	for id, t := range s.direct {
+		err := s.checkDial(t.dest, peers)
+		switch err {
+		case errNotWhitelisted, errSelf:
+			log.Warn("Removing direct dial candidate", "id", t.dest.ID(), "addr", &net.TCPAddr{IP: t.dest.IP(), Port: t.dest.TCP()}, "err", err)
+			delete(s.direct, t.dest.ID())
+		case nil:
+			s.dialing[id] = t.flags
+			newtasks = append(newtasks, t)
+		}
+	}
+
+	// compute connected
+	connected := map[string]map[enode.ID]struct{}{}
+	for _, g := range s.group {
+		connected[g.name] = map[enode.ID]struct{}{}
+	}
+
+	for id := range peers {
+		for _, g := range s.group {
+			if _, ok := g.nodes[id]; ok {
+				connected[g.name][id] = struct{}{}
+			}
+		}
+	}
+
+	for id := range s.dialing {
+		for _, g := range s.group {
+			if _, ok := g.nodes[id]; ok {
+				connected[g.name][id] = struct{}{}
+			}
+		}
+	}
+
+	groupNodes := map[enode.ID]*enode.Node{}
+	for _, g := range s.group {
+		for _, n := range g.nodes {
+			if uint64(len(connected[g.name])) >= g.num {
+				break
+			}
+			err := s.checkDial(n, peers)
+			switch err {
+			case errNotWhitelisted, errSelf:
+				log.Warn("Removing group dial candidate", "id", n.ID(), "addr", &net.TCPAddr{IP: n.IP(), Port: n.TCP()}, "err", err)
+				delete(g.nodes, n.ID())
+			case nil:
+				groupNodes[n.ID()] = n
+				connected[g.name][n.ID()] = struct{}{}
+			}
+		}
+	}
+	for _, n := range groupNodes {
+		addDial(groupDialedConn, n)
+	}
+
 	// If we don't have any peers whatsoever, try to dial a random bootnode. This
 	// scenario is useful for the testnet (and private networks) where the discovery
 	// table might be full of mostly bad peers, making it hard to find good ones.