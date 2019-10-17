40c3cd4223c084e66162d48f31514b38ae95c1e0
Jhih-Ming Huang <jm@byzantine-lab.io>
2019-10-18 00:27:00 +0800

    print vm config

--- core/state_processor.go
+++ core/state_processor.go
@@ -24,6 +24,7 @@ import (
 	"github.com/tangerine-network/go-tangerine/core/types"
 	"github.com/tangerine-network/go-tangerine/core/vm"
 	"github.com/tangerine-network/go-tangerine/crypto"
+	"github.com/tangerine-network/go-tangerine/log"
 	"github.com/tangerine-network/go-tangerine/params"
 )
 
@@ -86,6 +87,8 @@ func (p *StateProcessor) Process(block *types.Block, statedb *state.StateDB, cfg
 // for the transaction, gas used and an error if the transaction failed,
 // indicating the block was invalid.
 func ApplyTransaction(config *params.ChainConfig, bc ChainContext, author *common.Address, gp *GasPool, statedb *state.StateDB, header *types.Header, tx *types.Transaction, usedGas *uint64, cfg vm.Config) (*types.Receipt, uint64, error) {
+	logger := log.New("cfg", cfg)
+	logger.Debug("============", "cfg.Debug", cfg.Debug)
 	msg, err := tx.AsMessage(types.MakeSigner(config, header.Number))
 	if err != nil {
 		return nil, 0, err
